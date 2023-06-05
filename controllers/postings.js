2b75a3d18d20310976146c5f01161950514a002f
Daniel Mathews <daniel.g.mathews@gmail.com>
2023-06-05 12:27:18 -0400

    add response logic to create controller function

--- controllers/postings.js
+++ controllers/postings.js
@@ -5,6 +5,7 @@ async function create (req, res) {
     req.body.creatorId = req.user.profile.id
     const posting = await Posting.create(req.body)
     console.log(posting.toJSON())
+    res.status(201).json(posting)
   } catch (error) {
     console.log(error)
     res.status(500).json(error)
